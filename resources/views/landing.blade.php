9612662a9954bfc1d78c5fb453c0f759c243aa06
dylanonfb <dylanonfb@gmail.com>
2021-08-17 23:39:34 +0530

    laravel testing

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+
+    <title>Home</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Welcome to my Laravel website</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, porro quaerat harum unde beatae similique laudantium velit mollitia cupiditate possimus. Laudantium amet vitae consectetur adipisci harum quibusdam quas illum eos.</p>
+    </div>
+</body>
+
+</html>
