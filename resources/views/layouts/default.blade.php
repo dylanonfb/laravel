ddfc17a74f7e4c6d6ba3fc73cd7a965b022b868b
dylanonfb <dylanonfb@gmail.com>
2021-08-30 16:33:40 +0530

    app name from env

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,7 +12,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
-    <title>My Laravel App</title>
+    <title>{{$APP_NAME}}</title>
 </head>
 
 <body>
