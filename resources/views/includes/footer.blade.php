0d40090f16e7968a19ea861328a55fd543e55b30
dylanonfb <dylanonfb@gmail.com>
2021-08-30 16:30:45 +0530

    footer .

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
       <div class="container text-center">
-        <span class="text-muted">Copyright 2021 Changerline</span>
+        <span class="text-muted">Copyright 2021 Changerline.</span>
       </div>
     </footer>
